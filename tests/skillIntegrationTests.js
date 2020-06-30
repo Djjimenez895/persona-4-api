5da1abc183693fc891e1c03eb793af5b46949b19
Davin Jimenez <davinjjimenez@gmail.com>
2020-06-30 09:55:01 -0700

    Add '/api/' to test explanations for skill tests
    The URI end points were missing the '/api/' part of the their URI
    for the descriptions given in each test. I have added this so that
    the URI is accurate now.

--- tests/skillIntegrationTests.js
+++ tests/skillIntegrationTests.js
@@ -11,35 +11,35 @@ const Skill = mongoose.model('Skill');
 const agent = request.agent(app);
 
 describe('Skill CRUD Tests', () => {
-  it('Should return all skills in the database (/skills GET) ', (done) => {
+  it('Should return all skills in the database (/api/skills GET) ', (done) => {
     throw new Error('Needs to be implemented');
   });
 
-  it('Should return skills that cost mp (/skills/cost/mp GET) ', (done) => {
+  it('Should return skills that cost mp (/api/skills/cost/mp GET) ', (done) => {
     throw new Error('Needs to be implemented');
   });
 
-  it('Should return skills that cost hp (/skills/cost/hp GET) ', (done) => {
+  it('Should return skills that cost hp (/api/skills/cost/hp GET) ', (done) => {
     throw new Error('Needs to be implemented');
   });
 
-  it('Should return skills that cost hp (/skills/type/:skillType GET) ', (done) => {
+  it('Should return skills that cost hp (/api/skills/type/:skillType GET) ', (done) => {
     throw new Error('Needs to be implemented');
   });
 
-  it('Should return skills with the given prefix in their skill name (/skills/name/:skillName GET) ', (done) => {
+  it('Should return skills with the given prefix in their skill name (/api/skills/name/:skillName GET) ', (done) => {
     throw new Error('Needs to be implemented');
   });
 
-  it('Should return skills that have a power >= the given power level (/skills/power/:powerLevel GET) ', (done) => {
+  it('Should return skills that have a power >= the given power level (/api/skills/power/:powerLevel GET) ', (done) => {
     throw new Error('Needs to be implemented');
   });
 
-  it('Should return skills with an accuracy >= the one given (/skills/accuracy/:accuracyLevel GET) ', (done) => {
+  it('Should return skills with an accuracy >= the one given (/api/skills/accuracy/:accuracyLevel GET) ', (done) => {
     throw new Error('Needs to be implemented');
   });
 
-  it('Should return skills with a critical chance >= the one given (/skills/critical/:criticalChance GET) ', (done) => {
+  it('Should return skills with a critical chance >= the one given (/api/skills/critical/:criticalChance GET) ', (done) => {
     throw new Error('Needs to be implemented');
   });
 });
